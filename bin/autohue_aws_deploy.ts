13e591b215590a9ae825b13b1b15a768688d6b5a
Mikhail Dorfman <mikhaildorfman@gmail.com>
2023-01-31 10:13:01 -0500

    initial commit on new directory

---
+++ bin/autohue_aws_deploy.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AutoHueAwsDeployStack } from '../lib/autohue_aws_deploy-stack';
+import { TemplateConfig } from "../lib/template-configs";
+import { getRequiredContext } from "../lib/utils";
+import {Construct} from "constructs";
+
+
+const app = new cdk.App();
+
+const templateConfig : TemplateConfig ={
+    hueAddress: getRequiredContext(app, 'hueAddress'),
+    hueApiKey: getRequiredContext(app, 'hueApiKey'),
+    lightGroup: getRequiredContext(app, 'lightGroup'),
+    lambdaFunctions: getRequiredContext(app, 'lambdaFunctions')
+}
+
+new AutoHueAwsDeployStack(app, 'AutohueAwsDeployStack', templateConfig);
---
+++ lib/autohue_aws_deploy-stack.ts
@@ -0,0 +1,111 @@
+import * as cdk from 'aws-cdk-lib';
+import { aws_ssm as ssm} from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_events as events } from "aws-cdk-lib";
+import { aws_sqs as sqs } from "aws-cdk-lib";
+import { aws_apigateway as api} from "aws-cdk-lib";
+import { aws_iam as iam } from "aws-cdk-lib";
+import * as functions from "./functions";
+import {DataCollectionBuild} from "./data_collection";
+import { TemplateConfig } from "./template-configs";
+import { Construct } from 'constructs';
+import {update} from "aws-cdk-lib/cloud-assembly-schema/scripts/update-schema";
+
+export class AutoHueAwsDeployStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, templateConfig: TemplateConfig, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const hueApiKeyParam = new ssm.StringParameter(this, 'HueApiKeyParameter', {
+      description: 'Parameter for the Philips Hue API',
+      parameterName: 'hue-api-key',
+      stringValue: templateConfig.hueApiKey,
+    });
+
+    const modelBucket = new s3.Bucket(this, 'ModelBucket');
+
+    const cronRule = new events.Rule(this, 'TemperatureCronRule', {
+      schedule: events.Schedule.expression('cron(* 6-23 * * *)')
+    });
+
+    const [updaterFn, modelGeneratorFn] = this.createFunctions(
+        templateConfig.lambdaFunctions.modelGeneratorFnModuleName,
+        templateConfig.lambdaFunctions.temperatureCycleFnModuleName,
+        templateConfig.lambdaFunctions.updaterFnModuleName,
+        modelBucket, templateConfig.hueAddress, hueApiKeyParam,
+        templateConfig.lightGroup, cronRule);
+
+    // create the api
+    const lightsApi = new api.RestApi(this, 'ServerlessLightsApi', {
+      restApiName: `lightsApi`
+    });
+    // separate resources and methods for each function that needs the api
+    const updaterApiResource = lightsApi.root.addResource('updater');
+    updaterApiResource.addMethod('PUT',
+        new api.LambdaIntegration(updaterFn), {
+          //apiKeyRequired: true,
+        });
+    const modelGeneratorApiResource = lightsApi.root.addResource('modelGenerator');
+    modelGeneratorApiResource.addMethod('PUT',
+        new api.LambdaIntegration(modelGeneratorFn), {
+          //apiKeyRequired: true
+        });
+    // permissions for the api to invoke functions
+    updaterFn.addPermission('PermitUpdaterFnInvocation', {
+      principal: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+      sourceArn: lightsApi.arnForExecuteApi('*'),
+    });
+    modelGeneratorFn.addPermission('PermitModelGeneratorFnInvocation', {
+      principal: new iam.ServicePrincipal('apigateway.amazonaws.com'),
+      sourceArn: lightsApi.arnForExecuteApi('*'),
+    });
+
+    if (templateConfig.lambdaFunctions.dbWriterFnModuleName !== undefined) {
+      console.log('hello2')
+      const dataCollection = new DataCollectionBuild(this, id,
+          templateConfig.lambdaFunctions.dbWriterFnModuleName
+          );
+      updaterFn.addToRolePolicy(new iam.PolicyStatement({
+        actions: [],
+        resources:[dataCollection.queue.queueArn]
+      }));
+      updaterFn.addToRolePolicy(new iam.PolicyStatement({
+        actions: ['sqs:SendMessage', 'sqs:GetQueueAttributes', 'sqs:GetQueueUrl',],
+        resources: [dataCollection.queue.queueArn,]
+      }));
+      updaterFn.addEnvironment(
+          "WRITE_QUEUE_NAME", dataCollection.queue.queueName
+    );
+    }
+    else {
+      console.log('hello1')
+      updaterFn.addEnvironment(
+          'WRITE_QUEUE_NAME', ''
+      )
+    }
+  }
+
+  private createFunctions(
+      modelGeneratorFnModuleName: string, temperatureCycleFnModuleName: string,
+      updaterFnModuleName: string, modelBucket: s3.Bucket,
+      address: string, hueApiKey: ssm.StringParameter,
+      lightGroup: string, cronTriggerEvent: events.Rule,
+  ){
+    const webParams: functions.WebParams = {
+      address: address,
+      hueApiKey: hueApiKey
+    }
+    const updateFn = functions.createUpdaterFn(
+        this, webParams, cronTriggerEvent, updaterFnModuleName,
+        );
+    const temperatureCycleFn = functions.createTemperatureCycleFn(
+        this, webParams, modelBucket, temperatureCycleFnModuleName, lightGroup, cronTriggerEvent
+    );
+    const modelGeneratorFn = functions.createModelGeneratorFn(
+        this, temperatureCycleFn, modelGeneratorFnModuleName, modelBucket
+    )
+
+    return [updateFn, modelGeneratorFn] as const;
+
+  }
+}
+
